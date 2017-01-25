16183fb8706d46036f6832fed539fa8ad321298d
Kabunga <Agaba.Kabunga@nov.com>
2017-01-25 08:55:28 -0600

    use $inject for Dependency injection

--- app/mainCtrl.js
+++ app/mainCtrl.js
@@ -1,22 +1,30 @@
 (function(){
 'use strict'
  angular.module("MyFirstApp")
-  .controller("mainCtrl",function($scope){
-    var vm = this;
-    vm.name ="";
-    vm.stringValue = 0;
+  .controller("mainCtrl",mainCtrl);
 
-    vm.displayStringValue  = function(){
-      var totalStringValue = calculateStringValue(vm.name);
-      vm.stringValue = totalStringValue;
-    }
+mainCtrl.$inject = ['$filter'];
 
-    function calculateStringValue(string){
-       var stringValue = 0;
-      for (var i = 0; i < string.length; i++) {
-            stringValue += string.charCodeAt(i);
-      }
-      return stringValue;
-    }
-  })
+  function mainCtrl($filter){
+        var vm = this;
+        vm.name ="";
+        vm.stringValue = 0;
+        vm.displayStringValue  = function(){
+              var totalStringValue = calculateStringValue(vm.name);
+              vm.stringValue = totalStringValue;
+        }
+        vm.upper = function(){
+          var upperCase = $filter('uppercase');
+          vm.name = upperCase(vm.name);
+        }
+        function calculateStringValue(string){
+             var stringValue = 0;
+              for (var i = 0; i < string.length; i++) {
+                    stringValue += string.charCodeAt(i);
+              }
+              return stringValue;
+        }
+  }
 })();
+
+  //use vm instead of $scope
